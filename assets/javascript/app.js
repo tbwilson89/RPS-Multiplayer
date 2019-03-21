0b19ef2e0f286796b8e25681b655cc7f0ae0f6ac
Trevor Wilson <trevorbwilson89@gmail.com>
2019-03-21 11:53:42 -0500

    Cleaning up style.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -143,23 +143,19 @@ connectionsList.on('value', (snap)=>{
       })
     }
   })
+  //Remove users that have disconnected
+  $('#chat-active-users').children().each(function(){
+    if(!snap.val()[$(this).data('user').id]){
+      $(this).remove()
+    }
+  })
   for(key in snap.val()){
-    console.log('In key loop')
-    //Remove users that have disconnected
-    $('#chat-active-users').children().each(function(){
-      console.log($(this).data('user').id)
-      if(!snap.val()[$(this).data('user').id]){
-        $(this).remove()
-      }
-    })
     // Add new users
     if ($(`#${key}`).text() === ''){
-      console.log('Creating username chat element and appending for: ' + key)
       var displayUserDiv = $(`<div id=${key} class='card col-md-12' data-user='{"id": "${key}"}'>${snap.val()[key].name}</div>`)
       $('#chat-active-users').append(displayUserDiv)
       // Update usernames when they are changed
     } else if ($(`#${key}`).text() !== snap.val()[key].name){
-      console.log('Updating name for: ' + key)
       $(`#${key}`).text(snap.val()[key].name)
     }
   }
